9298ece4aea4fc5ca7973912b465cb0c543a60f0
Gautham Kumar <heygautham@gmail.com>
2018-08-25 17:18:54 +0530

    Ensures smooth closing of invalid websockets

--- server.py
+++ server.py
@@ -6,16 +6,20 @@ USERS = set()
 async def notify_all_users(message):
     await asyncio.wait([user.send(message) for user in USERS])
 
+async def close_connection(user):
+    USERS.remove(user)
+    print('connection closed for - ',user)
+
 async def on_connection(websocket, path):
     USERS.add(websocket)
+    print('connection established for - ',websocket)
     try:
         async for message in websocket:
             print(f"< {message}")
             await notify_all_users(message)
 
-    except:
-        USERS.remove(websocket)
-        print('connection closed for - ',websocket)
+    finally:
+        await close_connection(websocket)
 
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(on_connection, 'localhost', 12345))
